d8f9bdef954a65db26cadd841e22dba0362012b6
jithinoc <jithindotcom@gmail.com>
2016-10-29 23:00:55 +0530

    Added dense_layer

---
+++ trishula/layers/__init__.py
@@ -0,0 +1,2 @@
+
+from .dense_layer import DenseLayer 
---
+++ trishula/layers/dense_layer.py
@@ -0,0 +1,10 @@
+
+from thrishula.abstracts import Layer
+
+class DenseLayer(Layer):
+
+	def __init__(self):
+		pass
+
+	def feedforward(self):
+		pass
