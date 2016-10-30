1466a6d08ce0cabd8dae64f020324c9e1785c518
jithinoc <jithindotcom@gmail.com>
2016-10-30 11:34:32 +0530

    Implemented denselayer feedforward logic

--- trishula/layers/dense_layer.py
+++ trishula/layers/dense_layer.py
@@ -1,10 +1,22 @@
 
+from tensorflow import tf 
 from trishula.abstracts import Layer
 
 class DenseLayer(Layer):
 
-	def __init__(self):
-		pass
+	def __init__(self, size, w_initializer=None, activ_fn=None):
+		if w_initializer:
+			W_value, b_value = w_initializer(size=size, activ_fn=activ_fn)
+		else:
+			W_value, b_value = tf.zeros(size), tf.zeros([size[1]])
 
-	def feedforward(self):
-		pass
+		self.W = tf.Variable(W_value)
+		self.b = tf.Variable(b_value)
+
+		self.activ_fn = activ_fn
+
+		self.params = [self.W, self.b]
+
+	def feedforward(self, input):
+		pre_activation = tf.matmul(input, self.W) + self.b
+		return self.activ_fn(pre_activation) if self.activ_fn else pre_activation
