6a0164f050fea6097fa9c8a76de5b65aab011afe
jithinoc <jithindotcom@gmail.com>
2016-11-08 16:20:32 +0530

    added one_hot encoding mnist

--- trishula/datasets/mnist.py
+++ trishula/datasets/mnist.py
@@ -1,5 +1,5 @@
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-def load_data(dir, one_hot=True):
-	return input_data.read_data_sets(dir, one_hot)
+def load_data(dirname, one_hot=True):
+	return input_data.read_data_sets(dirname, one_hot=one_hot)
