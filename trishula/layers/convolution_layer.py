fc17d424e5d8787034c441192f0813cb6cc1cac1
jithinoc <jithindotcom@gmail.com>
2016-11-12 23:35:57 +0530

    Removed activ_fn param

--- trishula/layers/convolution_layer.py
+++ trishula/layers/convolution_layer.py
@@ -12,13 +12,11 @@ class Convolution2DLayer(Layer):
 			strides=1, 
 			padding='SAME', 
 			w_initializer=weight_initializers.truncated_normal,
-			b_initializer=weight_initializers.constant, 
-			activ_fn=None):
+			b_initializer=weight_initializers.constant):
 		self.W = w_initializer(self.__class__.__name__+'_'+str(id(self))+'_W', filter_shape)
 		self.b = b_initializer(self.__class__.__name__+'_'+str(id(self))+'_b', filter_shape[-1:])
 
 		self.input_shape = [-1] + list(input_shape[1:])
-		self.activ_fn = activ_fn
 		self.strides = [1, strides, strides, 1]
 		self.padding = padding
 
@@ -32,5 +30,4 @@ class Convolution2DLayer(Layer):
 						strides=self.strides, 
 						padding=self.padding
 						)
-		pre_activation = pre_activation + self.b
-		return self.activ_fn(pre_activation) if self.activ_fn else pre_activation
+		return pre_activation + self.b
--- trishula/layers/dense_layer.py
+++ trishula/layers/dense_layer.py
@@ -9,14 +9,12 @@ class DenseLayer(Layer):
 			self, 
 			shape, 
 			w_initializer=weight_initializers.gloret,
-			b_initializer=weight_initializers.gloret, 
-			activ_fn=None):
+			b_initializer=weight_initializers.gloret):
 		self.W = w_initializer(self.__class__.__name__+'_'+str(id(self))+'_W', shape)
-		self.b = w_initializer(self.__class__.__name__+'_'+str(id(self))+'_b', shape[-1:])
-		self.activ_fn = activ_fn
+		self.b = b_initializer(self.__class__.__name__+'_'+str(id(self))+'_b', shape[-1:])
 
 		self.params = [self.W, self.b]
 
 	def feedforward(self, input):
 		pre_activation = tf.matmul(input, self.W) + self.b
-		return self.activ_fn(pre_activation) if self.activ_fn else pre_activation
+		return pre_activation
