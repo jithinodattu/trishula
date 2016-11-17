831204b0205ad78879cdb1121c25ae79d22a4b44
jithinoc <jithindotcom@gmail.com>
2016-11-17 19:15:05 +0530

    Added sparse_softamax_cross_entropy

--- trishula/utils/error_functions.py
+++ trishula/utils/error_functions.py
@@ -6,3 +6,6 @@ def cross_entropy(y, y_):
 
 def softmax_cross_entropy(y, y_):
 	return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+
+def sparse_softmax_cross_entropy(y, y_):
+	return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y, y_))
