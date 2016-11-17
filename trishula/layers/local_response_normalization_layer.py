2faa6212f72826ee9f64345043e1535902a8a93a
jithinoc <jithindotcom@gmail.com>
2016-11-17 14:25:57 +0530

    Implemented alexnet for cifar10
    Added sparse_softamax_cross_entropy
    
    changed error function to sparse_softmax_cross_entropy
    
    Added local_response_normalization_layer
    
    sum all losses
    
    one_hot encoding for labels
    
    Added LocalResponseNormalizationLayer, changed error to softmax_cross_entropy; batch size=100
    
    Added LRM layer
    
    Using tensor data

---
+++ trishula/layers/local_response_normalization_layer.py
@@ -0,0 +1,20 @@
+
+import tensorflow as tf
+from trishula.abstracts.layer import Layer
+
+class LocalResponseNormalizationLayer(Layer):
+
+	def __init__(self, depth_radius, bias=1.0, alpha=0.001/9.0, beta=0.75):
+		self.depth_radius = depth_radius
+		self.bias = bias
+		self.alpha = alpha
+		self.beta = beta
+
+	def feedforward(self, input):
+		return tf.nn.lrn(
+					input, 
+					bias=self.bias, 
+					depth_radius=self.depth_radius, 
+					alpha=self.alpha, 
+					beta=self.beta
+					)
