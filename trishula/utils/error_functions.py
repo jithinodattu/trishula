eab3149db0f3eea4541307c3b621a29e3abc700a
jithinoc <jithindotcom@gmail.com>
2016-11-18 18:27:53 +0530

    sum all losses

--- trishula/utils/error_functions.py
+++ trishula/utils/error_functions.py
@@ -8,4 +8,6 @@ def softmax_cross_entropy(y, y_):
 	return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
 
 def sparse_softmax_cross_entropy(y, y_):
-	return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y, y_))
+	cross_entropy_mean = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y, y_))
+	tf.add_to_collection('losses', cross_entropy_mean)
+	return tf.add_n(tf.get_collection('losses'), name='total_loss')
