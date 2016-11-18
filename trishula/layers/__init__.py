fad81b885e7e91e3c48f72f601c644f93c015004
jithinoc <jithindotcom@gmail.com>
2016-11-18 18:31:00 +0530

    Added LRM layer

--- trishula/layers/__init__.py
+++ trishula/layers/__init__.py
@@ -6,3 +6,4 @@ from .pooling_layer import MaxPooling2DLayer
 from .flatten_layer import FlattenLayer
 from .dropout_layer import DropOutLayer
 from .activation_layer import ActivationLayer
+from .local_response_normalization_layer import LocalResponseNormalizationLayer
