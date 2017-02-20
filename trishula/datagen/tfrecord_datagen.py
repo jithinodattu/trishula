4f85fa27b445c3d64673e6d124a9bdff86d215ca
jithinoc <jithindotcom@gmail.com>
2017-02-20 18:35:31 +0530

    Added tfrecord_datagen generic class

---
+++ trishula/datagen/tfrecord_datagen.py
@@ -0,0 +1,10 @@
+
+from trishula.abstracts import DataGen
+
+class TFRecordDataGen(DataGen):
+
+  def __init__(self, TFRecord_dir, dataset_name):
+    pass
+
+  def generate_from_TFRecords(partition_rule, shuffle=True):
+    pass
