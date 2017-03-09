4187c22ba62279644296afb80db043b3bb2e5262
jithinoc <jithindotcom@gmail.com>
2017-03-09 17:30:27 +0530

    download and process cifar10 if not present in dir

--- trishula/datagen/tfrecord_datagen.py
+++ trishula/datagen/tfrecord_datagen.py
@@ -1,4 +1,5 @@
 
+import os
 import tensorflow as tf
 
 from trishula.abstracts import DataGen
@@ -17,10 +18,10 @@ def read_tfrecords(TFRecord_dirpath):
                                     'height': tf.FixedLenFeature([], tf.int64),
                                     'width': tf.FixedLenFeature([], tf.int64),
                                     'depth': tf.FixedLenFeature([], tf.int64),
-                                    'image': tf.FixedLenFeature([], tf.string),
+                                    'image': tf.FixedLenFeature([], tf.float32),
                                     'label': tf.FixedLenFeature([], tf.int64) 
                                    })
-  image = tf.decode_raw(features['image'], tf.uint8)
+  image = features['image']
   height = tf.cast(features['height'], tf.int32)
   width = tf.cast(features['width'], tf.int32)
   depth = tf.cast(features['depth'], tf.int32)
@@ -28,10 +29,11 @@ def read_tfrecords(TFRecord_dirpath):
 
   images, labels = tf.train.shuffle_batch(
                                           [image, label], 
-                                          batch_size=10, 
+                                          batch_size=100, 
                                           capacity=30, 
                                           num_threads=2, 
                                           min_after_dequeue=10)
+  return images, labels
 
 class DatasetContainer(object):
   pass
@@ -43,11 +45,23 @@ class Partition(object):
 class TFRecordDataGen(DataGen):
 
   def __init__(self, TFRecord_dirpath):
-    training_partition = Partition()
-    training_partition.next_batch = read_tfrecords(TFRecord_dirpath)
-    self.dataset = DatasetContainer()
-    self.dataset.train = training_partition
-    self.dataset.test = training_partition
+    #pipeline for trainset
+    train_partition = Partition()
+    train_dirpth = os.path.join(TFRecord_dirpath, 'train')
+    train_partition.next_batch = read_tfrecords(train_dirpth)
+    #pipeline for devset
+    dev_partition = Partition()
+    dev_dirpth = os.path.join(TFRecord_dirpath, 'dev')
+    dev_partition.next_batch = read_tfrecords(dev_dirpth)
+    #pipeline for testset
+    test_partition = Partition()
+    test_dirpth = os.path.join(TFRecord_dirpath, 'test')
+    test_partition.next_batch = read_tfrecords(test_dirpth)
 
-  def generate_dataset(partition_rule, shuffle=True):
+    self.dataset = DatasetContainer()
+    self.dataset.train = train_partition
+    self.dataset.dev = dev_partition
+    self.dataset.test = test_partition
+
+  def generate_from_TFRecords(self, shuffle=True):
     return self.dataset
